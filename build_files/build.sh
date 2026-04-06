fa74133e603d0a02b9bd0eb26b559f71f0f0ea52
Dobli <github@mailvoid.de>
2026-04-07 00:54:14 +0200

    Try manually running post install scripts

--- build_files/build.sh
+++ build_files/build.sh
@@ -28,23 +28,17 @@ dnf5 install -y pnpm
 # CachyOS Kernel
 echo 'Enable SElinux Kernel policy'
 setsebool -P domain_kernel_load_modules on
+# Add copr for kernel
 dnf5 -y copr enable bieszczaders/kernel-cachyos
-
-# Run depmod before dracut is triggered by the %posttrans scriptlet
-cat > /usr/lib/kernel/install.d/04-depmod.install << 'EOF'
-#!/bin/bash
-COMMAND="$1"
-KERNEL_VERSION="$2"
-if [[ "$COMMAND" == "add" ]]; then
-    depmod -a "$KERNEL_VERSION"
-fi
-EOF
-chmod +x /usr/lib/kernel/install.d/04-depmod.install
-
+# Don't execute post install scripts to handle them manually
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-dnf5 -y install kernel-cachyos
-
-rm /usr/lib/kernel/install.d/04-depmod.install
+dnf5 -y install kernel-cachyos --setopt=tsflags=noscripts
+# Build the module dependency map that dracut needs
+KVER=$(rpm -q --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos-core)
+depmod -a "$KVER"
+# Generate the initramfs manually
+dracut --force --kver "$KVER" "/boot/initramfs-${KVER}.img"
+# Disable copr for kernel
 dnf5 -y copr disable bieszczaders/kernel-cachyos
 
 # Use a COPR Example:
